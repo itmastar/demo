c4010867de6068923ea9068f054cee2eca5ef459
Phani Madiraju <phanimkms1@gmail.com>
2023-08-31 18:05:37 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/")
+    public String Demo(){
+        return "Today I developed a RESTFul API";
+    }
+}
